ed219eba00c6421e2106a0fd6467f675e9e4d3d8
Ghanahshyam <kga.shyam@gmail.com>
2020-12-01 17:28:41 +0530

    implemented firebase uril, store authenticated use to firebase store

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
-import './sign-in-and-sign-up.styles.scss';
+import SignUp from "../../components/sing-up/sing-up.component";
+
+import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
-    <SignIn>
-    </SignIn>
+    <SignIn></SignIn>
+    <SignUp></SignUp>
   </div>
-)
+);
 
 export default SignInAndSignUpPage;
